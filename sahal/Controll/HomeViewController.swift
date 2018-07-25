faff402169a178ca23d8e1debb1e5fd75c9eb44a
fatooaj <fatima-aljaber@hotmail.com>
2018-07-25 16:43:45 +0300

    retrive data from database to the table

--- sahal/Controll/HomeViewController.swift
+++ sahal/Controll/HomeViewController.swift
@@ -5,22 +5,21 @@
 //  Created by Fatima Aljaber on 7/15/18.
 //  Copyright © 2018 wadimakkah. All rights reserved.
 //
-
 import UIKit
-
+import Firebase
 class HomeViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
-    @IBOutlet var changesegment: UISegmentedControl!
     
+    // Database
+    var databaseReference : DatabaseReference!
+    // var currentUser = Auth.auth().currentUser?
+    var Products = [Product]()
+    
+    @IBOutlet var changesegment: UISegmentedControl!
     @IBOutlet var tableproduct: UITableView!
-    @IBAction func ProductCatigorie(_ sender: UISegmentedControl){
-        tableproduct.reloadData()
-    }
+    
     @IBAction func BuyIconButton(_ sender: Any) {
     }
     
-    
-    @IBOutlet var shadowLayer: UIView!
-    @IBOutlet var mainBackground: UIView!
     var titleOfproduct = ["cars category 312321 camere germani 3123213","cars category 312321 camere germani 3123213","cars category 312321 camere germani 3123213","cars category 312321 camere germani 3123213","cars category 312321 camere germani 3123213","cars category 312321 camere germani 3123213","cars category 312321 camere germani 3123213"]
     var SellerName = ["Cars company","hope company", "Hassan company","Cars company","hope company", "Ahmed company","Cars company"]
     var cityname = ["Makkah","Jeddah","Madina","Dammam","Makkah","Jeddah","Madina"]
@@ -32,35 +31,44 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
     var productimg3 = ["4.jpg","4.jpg","4.jpg","4.jpg","4.jpg","4.jpg","4.jpg"]
     var productimg4 = ["5.jpg","5.jpg","5.jpg","5.jpg","5.jpg","3.jpg","4.jpg"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Database
+        databaseReference = Database.database().reference()
+        fetchUsers()
+    }
     
     
     
     
+    @IBAction func ProductCatigorie(_ sender: UISegmentedControl){
+        tableproduct.reloadData()
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch changesegment.selectedSegmentIndex
         {
         case 0:
-            return titleOfproduct.count
+            return Products.count
         case 1:
-            return titleOfproduct.count
+            return Products.count
         case 2:
-            return titleOfproduct.count
+            return Products.count
         case 3:
-            return titleOfproduct.count
+            return Products.count
         default:
             break;
         }
-        
         return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "productcell", for: indexPath) as! ProductTableViewCell
-        let title = titleOfproduct[indexPath.row]
-        let Sellername = SellerName[indexPath.row]
-        let cityName = cityname[indexPath.row]
-        let productprice = Price[indexPath.row]
+        let title = Products[indexPath.row].producttitle
+        //  let Sellername = Products[indexPath.row].title
+        //   let cityName = cityname[indexPath.row]
+        let productprice = Products[indexPath.row].Price
         // let Loctionicon = locationIcon[indexPath.row]
         // let carticon = shoppingIcon[indexPath.row]
         
@@ -69,52 +77,33 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
         case 0:
             // SHOWING THE HOME VIEW
             let Productimg = productimg[indexPath.row]
-            //            cell.titleproduct?.text = title
+            cell.titleproduct?.text = title
+            print(cell.titleproduct?.text)
             //            cell.sellername?.text = Sellername
             //            cell.city?.text = cityName
-            // cell.price?.text = productprice
+            cell.price?.text = productprice
             //    cell.locationicon?.image = UIImage(named: "location.png")
             // cell.ShoppingIcon?.image = UIImage(named: "shopp")
             cell.productimage?.image = UIImage(named: Productimg)
             //        //Tabel cell Style
-            cell.mainBackground.layer.cornerRadius = 10
-            cell.mainBackground.layer.masksToBounds = true
-            cell.shadowLayer.layer.masksToBounds = false
-            cell.shadowLayer.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(3))
-            cell.mainBackground.layer.backgroundColor = UIColor.white.cgColor
-            cell.shadowLayer.layer.shadowColor = UIColor.gray.cgColor
-            cell.shadowLayer.layer.shadowOpacity = 0.30
-            cell.shadowLayer.layer.shadowRadius = 10
-            cell.shadowLayer.layer.shadowPath = UIBezierPath(roundedRect: cell.shadowLayer.bounds, byRoundingCorners:.allCorners, cornerRadii: CGSize(width: 8, height: 8)).cgPath
-            cell.shadowLayer.layer.shouldRasterize = true
-            cell.shadowLayer.layer.rasterizationScale = UIScreen.main.scale
-            cell.selectionStyle = .none
+            cellDesign(cell: cell)
             
             
         case 1:
             // SHOWING THE PROFILE VIEW
             let Productimg = productimg2[indexPath.row]
-            //            cell.titleproduct?.text = title
+            cell.titleproduct?.text = title
+            
             //            cell.sellername?.text = Sellername
             //            cell.city?.text = cityName
-            //   cell.price?.text = productprice
+            cell.price?.text = productprice
             //    cell.locationicon?.image = UIImage(named: "location.png")
             //   cell.ShoppingIcon?.image = UIImage(named: "shopp")
             cell.productimage?.image = UIImage(named: Productimg)
             
             //        //Tabel cell Style
-            cell.mainBackground.layer.cornerRadius = 10
-            cell.mainBackground.layer.masksToBounds = true
-            cell.shadowLayer.layer.masksToBounds = false
-            cell.shadowLayer.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(3))
-            cell.mainBackground.layer.backgroundColor = UIColor.white.cgColor
-            cell.shadowLayer.layer.shadowColor = UIColor.gray.cgColor
-            cell.shadowLayer.layer.shadowOpacity = 0.30
-            cell.shadowLayer.layer.shadowRadius = 10
-            cell.shadowLayer.layer.shadowPath = UIBezierPath(roundedRect: cell.shadowLayer.bounds, byRoundingCorners:.allCorners, cornerRadii: CGSize(width: 8, height: 8)).cgPath
-            cell.shadowLayer.layer.shouldRasterize = true
-            cell.shadowLayer.layer.rasterizationScale = UIScreen.main.scale
-            cell.selectionStyle = .none
+            cellDesign(cell: cell)
+            
             
         case 2:
             // SHOWING THE SETTINGS VIEW
@@ -128,18 +117,7 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
             cell.productimage?.image = UIImage(named: Productimg)
             
             //        //Tabel cell Style
-            cell.mainBackground.layer.cornerRadius = 10
-            cell.mainBackground.layer.masksToBounds = true
-            cell.shadowLayer.layer.masksToBounds = false
-            cell.shadowLayer.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(3))
-            cell.mainBackground.layer.backgroundColor = UIColor.white.cgColor
-            cell.shadowLayer.layer.shadowColor = UIColor.gray.cgColor
-            cell.shadowLayer.layer.shadowOpacity = 0.30
-            cell.shadowLayer.layer.shadowRadius = 10
-            cell.shadowLayer.layer.shadowPath = UIBezierPath(roundedRect: cell.shadowLayer.bounds, byRoundingCorners:.allCorners, cornerRadii: CGSize(width: 8, height: 8)).cgPath
-            cell.shadowLayer.layer.shouldRasterize = true
-            cell.shadowLayer.layer.rasterizationScale = UIScreen.main.scale
-            cell.selectionStyle = .none
+            cellDesign(cell: cell)
         case 3:
             let Productimg = productimg4[indexPath.row]
             //            cell.titleproduct?.text = title
@@ -150,33 +128,31 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
             //    cell.ShoppingIcon?.image = UIImage(named: "shopp")
             cell.productimage?.image = UIImage(named: Productimg)
             
-            //        //Tabel cell Style
-            cell.mainBackground.layer.cornerRadius = 10
-            cell.mainBackground.layer.masksToBounds = true
-            cell.shadowLayer.layer.masksToBounds = false
-            cell.shadowLayer.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(3))
-            cell.mainBackground.layer.backgroundColor = UIColor.white.cgColor
-            cell.shadowLayer.layer.shadowColor = UIColor.gray.cgColor
-            cell.shadowLayer.layer.shadowOpacity = 0.30
-            cell.shadowLayer.layer.shadowRadius = 10
-            cell.shadowLayer.layer.shadowPath = UIBezierPath(roundedRect: cell.shadowLayer.bounds, byRoundingCorners:.allCorners, cornerRadii: CGSize(width: 8, height: 8)).cgPath
-            cell.shadowLayer.layer.shouldRasterize = true
-            cell.shadowLayer.layer.rasterizationScale = UIScreen.main.scale
-            cell.selectionStyle = .none
+            //Tabel cell Style
+            cellDesign(cell: cell)
+            
         default:
             break;
         }
-        
-        
-        
         return cell
+    }
+    func cellDesign(cell : ProductTableViewCell){
         
+        //        //Tabel cell Style
+        cell.mainBackground.layer.cornerRadius = 10
+        cell.mainBackground.layer.masksToBounds = true
+        cell.shadowLayer.layer.masksToBounds = false
+        cell.shadowLayer.layer.shadowOffset = CGSize(width: CGFloat(0), height: CGFloat(3))
+        cell.mainBackground.layer.backgroundColor = UIColor.white.cgColor
+        cell.shadowLayer.layer.shadowColor = UIColor.gray.cgColor
+        cell.shadowLayer.layer.shadowOpacity = 0.30
+        cell.shadowLayer.layer.shadowRadius = 10
+        cell.shadowLayer.layer.shadowPath = UIBezierPath(roundedRect: cell.shadowLayer.bounds, byRoundingCorners:.allCorners, cornerRadii: CGSize(width: 8, height: 8)).cgPath
+        cell.shadowLayer.layer.shouldRasterize = true
+        cell.shadowLayer.layer.rasterizationScale = UIScreen.main.scale
+        cell.selectionStyle = .none
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -184,7 +160,19 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
     }
     
     
-    
+    func fetchUsers(){
+        
+        self.databaseReference.child("items").observe(.childAdded) { (snapshots:DataSnapshot) in
+            if let dectionary = snapshots.value as? [String:AnyObject] {
+                print(dectionary)
+                let item = Product(dectionary: dectionary)
+                self.Products.append(item)
+                print(self.Products.count)
+                DispatchQueue.main.async {
+                    self.tableproduct.reloadData()
+                }
+            }
+        }}
     
     
 }
--- sahal/Model/Product.swift
+++ sahal/Model/Product.swift
@@ -5,7 +5,6 @@
 //  Created by Ghufran nour  on 05/11/1439 AH.
 //  Copyright © 1439 wadimakkah. All rights reserved.
 //
-
 import Foundation
 import UIKit
 
@@ -21,5 +20,27 @@ class Product {
         self.description = description
     }
     
+    var imgproduct = [String]()
+    var producttitle : String!
+    var productdescription : String!
+    var Price : String!
+    var City : String!
+    var Companyname : String!
+    var Category : String!
+    var status : String!
+    // var SellerID
     
+    
+    init (dectionary: [String: Any]) {
+        
+        //  self.img = img
+        self.producttitle = dectionary["name"] as? String
+        self.productdescription = dectionary["description"] as? String
+        self.Price = dectionary["price"] as? String
+        self.City = dectionary["City"] as? String
+        self.Companyname = dectionary["factoryName"] as? String
+        self.Category = dectionary["category"] as? String
+        self.status = dectionary["status"] as? String
+        
+    }
 }
